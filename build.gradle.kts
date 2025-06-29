18015d9f4fa4bd298ad1ca8ddebc29e64b3db0ac
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-06-29 08:27:54 -0600

    [FIX] remove unnecessary duplicate dependency for jackson-core

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 
     // JSON Handling
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
     // FIX: ELIMINADA la línea de 'io.javalin:javalin-json' que ya no existe.
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
