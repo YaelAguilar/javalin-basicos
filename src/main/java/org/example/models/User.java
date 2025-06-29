7d51916075a3e5ffd44693b537b9090c9189f232
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-06-29 00:57:29 -0600

    [ADD] user model

---
+++ src/main/java/org/example/models/User.java
@@ -0,0 +1,70 @@
+package org.example.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.time.LocalDateTime;
+
+public class User {
+    private int id;
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(int id, String username, String password, LocalDateTime createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
